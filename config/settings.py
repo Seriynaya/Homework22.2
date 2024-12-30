617986eba91a77b8dcdf0f5b5d23482a6a027391
Seriynaya <nagaewa.elina@gmail.com>
2024-12-30 18:34:32 +0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getemv('DATABASE_USER'),
+        'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
         'PORT': os.getenv('DATABASE_PORT'),
